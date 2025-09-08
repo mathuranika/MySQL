a27d636936fd2261ae29e35e928a6cd2218ed75d
Anika Mathur <111272336+mathuranika@users.noreply.github.com>
2025-09-08 21:58:03 +0530

    Add SQL insert statements for new users

---
+++ insertUsers.sql
@@ -0,0 +1,13 @@
+-- INSERT INTO users VALUES
+-- (1, 'alice@example.com', 'Alice', 'Female', '1995-05-14', DEFAULT);
+-- INSERT INTO users (email, name, date_of_birth, gender) VALUES
+-- ('anika@example.com', 'Anika', '2005-01-17','Female');
+-- INSERT INTO users (name, email, gender, date_of_birth) VALUES
+-- ('Bob', 'bob@example.com', 'Male', '1990-11-23'),
+-- ('Charlie', 'charlie@example.com', 'Other', '1988-02-17');
+
+INSERT INTO users (name, email, gender, date_of_birth) VALUES
+('David', 'david@example.com', 'Male', '2000-08-09'),
+('Eva', 'eva@example.com', 'Female', '1993-12-30');
+
+SELECT * FROM USERS
